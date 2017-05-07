95edd0dcc2b94e1a7373c3f84455840b07e87b6b
Educat <educatlab@gmail.com>
2017-05-07 12:14:26 +0700

    Membuat layout gabungan

---
+++ app/src/main/java/com/partcrime/agung/icebreakingiak/ActivityDepan.java
@@ -0,0 +1,13 @@
+package com.partcrime.agung.icebreakingiak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityDepan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_depan);
+    }
+}
